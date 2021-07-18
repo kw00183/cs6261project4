7ca5ad1d1ed3a44411a1aee3b56bc702d26573fd
kw00183 <kw00183@cs6261-vm07.uwg.westga.edu>
2021-07-17 23:20:18 -0400

    add in component elements and array for colorBrands values

--- src/app/resistance/calculator/calculator.component.ts
+++ src/app/resistance/calculator/calculator.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
 
-  constructor() { }
+//  public array: colorBand = [
+//    {color: 'black', figure: 0, multiply: 1, tolerance: null},
+//    {color: 'brown', figure: 1, multiply: 10, tolerance: 1},
+//    {color: 'red', figure: 2, multiply: 100, tolerance: 2},
+//    {color: 'orange', figure: 3, multiply: 1000, tolerance: null},
+//    {color: 'yellow', figure: 4, multiply: 10000, tolerance: null},
+//    {color: 'green', figure: 5, multiply: 100000, tolerance: 0.5},
+//    {color: 'blue', figure: 6, multiply: 1000000, tolerance: 0.25},
+//    {color: 'violet', figure: 7, multiply: 10000000, tolerance: 0.1},
+//    {color: 'grey', figure: 8, multiply: 100000000, tolerance: 0.05},
+//    {color: 'white', figure: 9, multiply: 1000000000, tolerance: null},
+//    {color: 'gold', figure: null, multiply: 0.1, tolerance: 5},
+//    {color: 'silver', figure: null, multiply: 0.01, tolerance: 10},
+//    {color: 'none', figure: null, multiply: null, tolerance: 20},
+//  ];
+
+  constructor() {}
 
   ngOnInit(): void {
   }
-
 }
