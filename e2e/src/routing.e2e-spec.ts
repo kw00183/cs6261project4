be6f0b6410df82ed6c36a80ad0726e8678b503f3
Kim Weible <kw00183@my.westga.edu>
2021-07-19 17:02:26 -0400

    add in color styles check on random band e2e test selection

--- e2e/src/routing.e2e-spec.ts
+++ e2e/src/routing.e2e-spec.ts
@@ -44,18 +44,23 @@ describe('Resistance App - Routing Tests', () => {
 
   it('should return the correct resistance and color for random bands', function() {
     element(by.css('#digit1')).element(by.cssContainingText('option', '3 orange')).click();
+    expect(element(by.id('band1color')).getCssValue('background-color')).toBe('rgb(255, 102, 0)');
     expect(element.all(by.tagName('p')).getText()).toContain('Resistance: 300 ± 1%');
 
     element(by.css('#digit2')).element(by.cssContainingText('option', '7 violet')).click();
+    expect(element(by.id('band2color')).getCssValue('background-color')).toBe('rgb(102, 0, 153)');
     expect(element.all(by.tagName('p')).getText()).toContain('Resistance: 370 ± 1%');
 
     element(by.css('#digit3')).element(by.cssContainingText('option', '6 blue')).click();
+    expect(element(by.id('band3color')).getCssValue('background-color')).toBe('rgb(0, 0, 153)');
     expect(element.all(by.tagName('p')).getText()).toContain('Resistance: 376 ± 1%');
 
     element(by.css('#multiplier')).element(by.cssContainingText('option', 'x100M grey')).click();
+    expect(element(by.id('band4color')).getCssValue('background-color')).toBe('rgb(102, 102, 102)');
     expect(element.all(by.tagName('p')).getText()).toContain('Resistance: 37.6G ± 1%');
 
     element(by.css('#tolerance')).element(by.cssContainingText('option', '± 4% yellow')).click();
+    expect(element(by.id('band5color')).getCssValue('background-color')).toBe('rgb(255, 255, 0)');
     expect(element.all(by.tagName('p')).getText()).toContain('Resistance: 37.6G ± 4%');
   });
 });
